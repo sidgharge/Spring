d61af80cc965bf53d32fa0cc38397c4a29ae30a9
sidgharge <ghargesiddharth@gmail.com>
2017-09-19 09:15:02 +0530

    Spring3

--- Spring3/src/main/java/com/bridgelabz/MyController.java
+++ Spring3/src/main/java/com/bridgelabz/MyController.java
@@ -2,13 +2,14 @@ package com.bridgelabz;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MyController {
 
-	@RequestMapping("/save")
+	@RequestMapping(value="/save.html", method=RequestMethod.POST)
 	public ModelAndView details(@RequestParam(name="name") String name, @RequestParam(name="hobby") String hobby){
 		ModelAndView modelAndView = new ModelAndView("welcome");
 		Student student = new Student();
--- Spring3/src/main/webapp/index.jsp
+++ Spring3/src/main/webapp/index.jsp
@@ -3,9 +3,9 @@
 <h2>Hello World!</h2>
 </body>
 
-<form action="save">
-Name: <input type="text" placeholder="name">
-Hobby: <input type="text" placeholder="hobby">
+<form action="save.html" method="post">
+Name: <input type="text" placeholder="name" name="name">
+Hobby: <input type="text" placeholder="hobby" name="hobby">
 <input type="submit" value="submit">
 </form>
 </html>
